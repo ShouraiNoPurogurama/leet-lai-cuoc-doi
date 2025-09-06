d3b15e3feece59cb3be135db9fd50f70282fded0
Nhat Anh <nhatanhtruong20@gmail.com>
2025-09-06 17:09:41 +0700

    done problem 27

---
+++ Top Interview 150/Array&String/27.remove-element.cs
@@ -0,0 +1,25 @@
+/*
+* @lc app=leetcode id=27 lang=csharp
+*
+* [27] Remove Element
+*/
+
+// @lc code=start
+public class Solution
+{
+    public int RemoveElement(int[] nums, int val)
+    {
+        int k = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != val)
+            {
+                nums[k++] = nums[i]; //ko quan trọng order
+            }
+        }
+
+        return k;
+    }
+}
+// @lc code=end
+
