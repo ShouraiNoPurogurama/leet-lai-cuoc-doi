b0ac19692f181816a5e9a857017e3ad291f881e9
Nhat Anh <nhatanhtruong20@gmail.com>
2025-09-09 22:23:38 +0700

    format problem 55

--- top-150-problems/Array&String/55.jump-game.cs
+++ top-150-problems/Array&String/55.jump-game.cs
@@ -10,12 +10,18 @@ public class Solution
     public bool CanJump(int[] nums)
     {
         int maxReach = 0;
+
         for (int i = 0; i < nums.Length; i++)
         {
-            if (i > maxReach) return false; 
-            maxReach = Math.Max(maxReach, i + nums[i]);
-            if (maxReach >= nums.Length - 1) return true; 
+            if (i > maxReach)
+                return false;
+
+            maxReach = maxReach > (nums[i] + i) ? maxReach : (nums[i] + i);
+
+            if (maxReach > nums.Length - 1)
+                return true;
         }
+
         return true;
     }
 }
